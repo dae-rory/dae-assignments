6502a7ae0bcbb9d68845182b6fa52b7364d602fe
Rory Fahy <fahyrory@gmail.com>
2023-12-15 19:08:04 -0500

    thing

--- python_1/no_comments.py
+++ python_1/no_comments.py
@@ -18,7 +18,7 @@ def generate_primes(limit):
 def main():
     upper_limit = int(input("Enter an upper limit to generate prime numbers: "))
     prime_numbers = generate_primes(upper_limit)
-    print(f"Prime numbers up to {upper_limit} are: {prime_numbers}")
+    print(f"Prime numbers 3up to {upper_limit} are: {prime_numbers}")
 
 
 if __name__ == "__main__":
